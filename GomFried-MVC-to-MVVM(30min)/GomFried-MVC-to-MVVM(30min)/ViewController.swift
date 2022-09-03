5196a87a1a488b5a7c89bdf7dca0556a2df2a229
OceanShape <oceanshape700@gmail.com>
2022-09-03 10:20:03 +0900

    Init project

---
+++ GomFried-MVC-to-MVVM(30min)/GomFried-MVC-to-MVVM(30min)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GomFried-MVC-to-MVVM(30min)
+//
+//  Created by 허태양 on 2022/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GomFried-MVC-to-MVVM(30min)/GomFried-MVC-to-MVVM(30min)UITests/GomFried_MVC_to_MVVM_30min_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GomFried_MVC_to_MVVM_30min_UITestsLaunchTests.swift
+//  GomFried-MVC-to-MVVM(30min)UITests
+//
+//  Created by 허태양 on 2022/09/03.
+//
+
+import XCTest
+
+class GomFried_MVC_to_MVVM_30min_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
